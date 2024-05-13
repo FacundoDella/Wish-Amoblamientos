7f8bf9c55e6f2290794b78905aceea7f76107350
Facundo Dellanegra <facundodiciembre25@gmail.com>
2024-05-13 11:04:48 -0300

    Seccion de Procutos terminada

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -20,82 +20,67 @@
     @include('includes-generales.navegacion')
     @include('includes-inicio.carrousel')
 
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ea fugiat illum nobis maiores id quidem
-        dignissimos quos quae obcaecati pariatur enim velit at, mollitia debitis nam consectetur, deleniti hic, itaque
-        autem a fugit iste facere? Saepe velit voluptates corporis qui hic accusantium pariatur assumenda vitae at esse
-        quos, ad dolorem voluptate? Ducimus quisquam accusantium repellendus consectetur est, nobis laborum? Blanditiis
-        molestias vero, perferendis nisi voluptatum architecto tempore nostrum, totam ab ipsa illum quisquam eius
-        impedit ratione ipsam obcaecati atque distinctio vitae repudiandae animi nam consequuntur. Cumque nobis
-        perferendis illum laudantium rem nihil porro dicta natus qui. Earum officiis cum reprehenderit cumque totam
-        adipisci omnis id vero laudantium nulla vel odio repellat itaque accusamus labore provident ea beatae, harum
-        asperiores consectetur eligendi! Officiis deserunt quia asperiores ab maxime hic accusamus inventore et labore
-        perspiciatis iusto quisquam voluptas, nemo nihil culpa, adipisci unde atque quas consequuntur numquam. Neque, ea
-        perspiciatis! Quod velit nihil maiores consequuntur assumenda incidunt ratione, beatae officia explicabo minima
-        reiciendis ex animi quidem? Similique eum blanditiis officiis quaerat fugit doloribus provident ab nobis
-        repellendus. Dolor odit, obcaecati necessitatibus, cumque maxime praesentium aliquam eaque tempora pariatur qui
-        atque accusamus illum quibusdam accusantium exercitationem sint facilis commodi provident facere distinctio.
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ea fugiat illum nobis maiores id quidem
-            dignissimos quos quae obcaecati pariatur enim velit at, mollitia debitis nam consectetur, deleniti hic,
-            itaque
-            autem a fugit iste facere? Saepe velit voluptates corporis qui hic accusantium pariatur assumenda vitae at
-            esse
-            quos, ad dolorem voluptate? Ducimus quisquam accusantium repellendus consectetur est, nobis laborum?
-            Blanditiis
-            molestias vero, perferendis nisi voluptatum architecto tempore nostrum, totam ab ipsa illum quisquam eius
-            impedit ratione ipsam obcaecati atque distinctio vitae repudiandae animi nam consequuntur. Cumque nobis
-            perferendis illum laudantium rem nihil porro dicta natus qui. Earum officiis cum reprehenderit cumque totam
-            adipisci omnis id vero laudantium nulla vel odio repellat itaque accusamus labore provident ea beatae, harum
-            asperiores consectetur eligendi! Officiis deserunt quia asperiores ab maxime hic accusamus inventore et
-            labore
-            perspiciatis iusto quisquam voluptas, nemo nihil culpa, adipisci unde atque quas consequuntur numquam.
-            Neque, ea
-            perspiciatis! Quod velit nihil maiores consequuntur assumenda incidunt ratione, beatae officia explicabo
-            minima
-            reiciendis ex animi quidem? Similique eum blanditiis officiis quaerat fugit doloribus provident ab nobis
-            repellendus. Dolor odit, obcaecati necessitatibus, cumque maxime praesentium aliquam eaque tempora pariatur
-            qui
-            atque accusamus illum quibusdam accusantium exercitationem sint facilis commodi provident facere distinctio.
-            dignissimos quos quae obcaecati pariatur enim velit at, mollitia debitis nam consectetur, deleniti hic,
-            itaque
-            autem a fugit iste facere? Saepe velit voluptates corporis qui hic accusantium pariatur assumenda vitae at
-            esse
-            quos, ad dolorem voluptate? Ducimus quisquam accusantium repellendus consectetur est, nobis laborum?
-            Blanditiis
-            molestias vero, perferendis nisi voluptatum architecto tempore nostrum, totam ab ipsa illum quisquam eius
-            impedit ratione ipsam obcaecati atque distinctio vitae repudiandae animi nam consequuntur. Cumque nobis
-            perferendis illum laudantium rem nihil porro dicta natus qui. Earum officiis cum reprehenderit cumque totam
-            adipisci omnis id vero laudantium nulla vel odio repellat itaque accusamus labore provident ea beatae, harum
-            asperiores consectetur eligendi! Officiis deserunt quia asperiores ab maxime hic accusamus inventore et
-            labore
-            perspiciatis iusto quisquam voluptas, nemo nihil culpa, adipisci unde atque quas consequuntur numquam.
-            Neque, ea
-            perspiciatis! Quod velit nihil maiores consequuntur assumenda incidunt ratione, beatae officia explicabo
-            minima
-            reiciendis ex animi quidem? Similique eum blanditiis officiis quaerat fugit doloribus provident ab nobis
-            repellendus. Dolor odit, obcaecati necessitatibus, cumque maxime praesentium aliquam eaque tempora pariatur
-            qui
-            atque accusamus illum quibusdam accusantium exercitationem sint facilis commodi provident facere distinctio.
-            dignissimos quos quae obcaecati pariatur enim velit at, mollitia debitis nam consectetur, deleniti hic,
-            itaque
-            autem a fugit iste facere? Saepe velit voluptates corporis qui hic accusantium pariatur assumenda vitae at
-            esse
-            quos, ad dolorem voluptate? Ducimus quisquam accusantium repellendus consectetur est, nobis laborum?
-            Blanditiis
-            molestias vero, perferendis nisi voluptatum architecto tempore nostrum, totam ab ipsa illum quisquam eius
-            impedit ratione ipsam obcaecati atque distinctio vitae repudiandae animi nam consequuntur. Cumque nobis
-            perferendis illum laudantium rem nihil porro dicta natus qui. Earum officiis cum reprehenderit cumque totam
-            adipisci omnis id vero laudantium nulla vel odio repellat itaque accusamus labore provident ea beatae, harum
-            asperiores consectetur eligendi! Officiis deserunt quia asperiores ab maxime hic accusamus inventore et
-            labore
-            perspiciatis iusto quisquam voluptas, nemo nihil culpa, adipisci unde atque quas consequuntur numquam.
-            Neque, ea
-            perspiciatis! Quod velit nihil maiores consequuntur assumenda incidunt ratione, beatae officia explicabo
-            minima
-            reiciendis ex animi quidem? Similique eum blanditiis officiis quaerat fugit doloribus provident ab nobis
-            repellendus. Dolor odit, obcaecati necessitatibus, cumque maxime praesentium aliquam eaque tempora pariatur
-            qui
-            atque accusamus illum quibusdam accusantium exercitationem sint facilis commodi provident facere distinctio.
-        </h1>
+    <div class="seccion1">
+        <div class="seccion1-info">
+            <h2>Especialistas en diseño, creación y colocación</h2>
+            <p>Somos una empresa con más de 20 años de experiencia en la entrega de productos de calidad.</p>
+        </div>
+        <div class="seccion1-boton">
+            <a href="" class="info-boton">Nuestros Asociados</a>
+        </div>
+    </div>
+
+    <div class="seccionProductos">
+        <div class="productosContenedor">
+            <div class="productosInfo">
+                <h2>Nuestros Productos</h2>
+                <p>¡Podes consultarnos hoy! Respuesta inmediata</p>
+            </div>
+
+            <div class="productosGrid">
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Cocina</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Sala de Estar</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Comedor</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Dormitorios</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Baño</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Oficina o Estudio</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Exterior</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Espacios Multifuncionales</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+                <a href="" class="productos">
+                    <h3 class="productosTitulo">Muebles Personalizados</h3>
+                    <img class="imagenProductos" src="{{ asset('imagenes/carrousel-3.jpg') }}" alt="Imagen Producto">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenedor-imagenFija">
+    </div>
+
 </body>
 
 </html>
