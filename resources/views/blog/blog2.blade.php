2a2c1072cb43abaf8407675d76b2f93e4777cb94
Facundo Dellanegra <facundodiciembre25@gmail.com>
2024-05-28 13:31:45 -0300

    Blog 2 Terminado

--- resources/views/blog/blog2.blade.php
+++ resources/views/blog/blog2.blade.php
@@ -1,1 +1,202 @@
-<h1>blog2</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Fuentes  --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+</head>
+
+
+@include('includes-generales.top')
+@include('includes-generales.navegacion')
+
+
+<body>
+    <header class="blogsHeader">
+        <h1>Consejos para el Cuidado y Mantenimiento de Muebles de Madera</h1>
+    </header>
+
+    <main>
+        <div class="contenedor">
+            <div class="contenedorBlog2Seccion">
+                <section class="blog2Seccion">
+                    <h2 class="blog2Titulo"><span>Sección 1: </span>Limpieza y Cuidado Diario de los Muebles de Madera
+                    </h2>
+                    <h4>Introducción</h4>
+                    <p>Mantener tus muebles de madera limpios y en buen estado es esencial para que luzcan siempre como
+                        nuevos. En esta sección, te daremos algunos consejos prácticos para la limpieza y el cuidado
+                        diario
+                        de tus muebles de madera.
+                    </p>
+
+                    <div class="blog2Consejos">
+                        <h3>Consejos Prácticos</h3>
+
+                        <ul class="blog2ConsejosLista">
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Polvo Regularmente: Utiliza un paño suave y seco para quitar el polvo de la
+                                    superficie
+                                    de
+                                    los muebles. Evita el uso de plumeros que pueden dejar rayones.</li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Utiliza Limpiadores Adecuados: Opta por productos específicos para madera o mezcla
+                                    una
+                                    solución suave de agua y jabón neutro. Aplica con un paño suave y seca
+                                    inmediatamente.
+                                </li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Protege contra el Agua: Nunca dejes líquidos sobre la superficie de la madera.
+                                    Utiliza
+                                    posavasos y limpia cualquier derrame inmediatamente para evitar manchas y daños.
+                                </li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Evita la Luz Directa del Sol: La exposición prolongada a la luz solar puede
+                                    descolorar y
+                                    dañar la madera. Coloca tus muebles lejos de ventanas o utiliza cortinas para
+                                    bloquear
+                                    los
+                                    rayos UV.</li>
+                            </div>
+                        </ul>
+                        <div class="blog2Conclusion">
+                            <h3>Conclusión</h3>
+                            <p>La limpieza y el cuidado diario son fundamentales para mantener la belleza y la
+                                durabilidad
+                                de tus muebles de madera. Siguiendo estos simples consejos, podrás disfrutar de tus
+                                muebles
+                                durante muchos años.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <div class="blog2ImagenesGrid">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/pañoSuave.jpg') }}" alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/limpiadorEspecifico.jpg') }}"
+                        alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/aguaEnMueble.jpg') }}" alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/muebleDecolorado.jpg') }}"
+                        alt="">
+                </div>
+            </div>
+            @include('includes-generales/separador')
+            <div class="contenedorBlog2Seccion">
+                <section class="blog2Seccion">
+                    <h2 class="blog2Titulo"><span>Sección 2: </span>Reparación y Mantenimiento Periódico de Muebles de
+                        Madera</h2>
+                    <h4>Introducción</h4>
+                    <p>Además de la limpieza diaria, los muebles de madera necesitan un mantenimiento periódico para
+                        conservar su estado óptimo. A continuación, te ofrecemos algunas recomendaciones para la
+                        reparación
+                        y el mantenimiento a largo plazo de tus muebles de madera.</p>
+
+                    <div class="blog2Consejos">
+                        <h3>Consejos de Mantenimiento</h3>
+
+                        <ul class="blog2ConsejosLista">
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Inspección Regular: Revisa tus muebles periódicamente en busca de arañazos, manchas
+                                    o
+                                    cualquier signo de desgaste. Detectar problemas a tiempo puede evitar daños mayores.
+                                </li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Lijado y Pulido: Si la superficie de la madera está rayada o desgastada, lija
+                                    suavemente
+                                    el
+                                    área afectada y aplica un barniz o cera para restaurar su brillo.</li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Reparación de Arañazos: Usa marcadores o ceras específicas para madera para cubrir
+                                    pequeños
+                                    arañazos. Para daños más profundos, considera la posibilidad de recurrir a un
+                                    profesional.
+                                </li>
+                            </div>
+                            <div class="blog2ContenedorLi">
+                                <svg class="completadoSVG" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""
+                                    data-id="12">
+                                    <path d="M20 6 9 17l-5-5"></path>
+                                </svg>
+                                <li>Aplicación de Aceites y Barnices: Cada cierto tiempo, aplica aceites o barnices
+                                    especiales
+                                    para nutrir y proteger la madera. Esto no solo mejora su apariencia sino que también
+                                    prolonga su vida útil.</li>
+                            </div>
+                        </ul>
+                        <div class="blog2Conclusion">
+                            <h3>Conclusión</h3>
+                            <p>El mantenimiento periódico es esencial para mantener tus muebles de madera en perfecto
+                                estado. Con estos consejos, podrás reparar pequeños daños y proteger tus muebles para
+                                que
+                                luzcan como nuevos durante más tiempo.</p>
+                        </div>
+                    </div>
+                </section>
+                <div class="blog2ImagenesGrid">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/muebleDesgastado.jpg') }}"
+                        alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/lijado.jpg') }}" alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/arañazos.jpg') }}" alt="">
+                    <img class="blog2Imagenes" src="{{ asset('imagenes/blog/blog2/barnices.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    @include('includes-generales.footer')
+</body>
